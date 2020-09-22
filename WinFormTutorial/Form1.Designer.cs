d6d7a1b7a8758f64d92b6a4b3c189765c9679515
sonohoshi <yd111544@gmail.com>
2020-09-22 20:00:15 +0900

    Label&Button&TextBox tutorial

--- WinFormTutorial/Form1.Designer.cs
+++ WinFormTutorial/Form1.Designer.cs
@@ -29,11 +29,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttbuttonbu
+            // 
+            this.label1.Location = new System.Drawing.Point(178, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 44);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "텍스트.";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(341, 185);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 53);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "버튼.";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox1.Location = new System.Drawing.Point(72, 142);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(142, 25);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(528, 287);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Fuck Me";
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
 
         #endregion
     }
--- WinFormTutorial/Form1.cs
+++ WinFormTutorial/Form1.cs
@@ -16,5 +16,11 @@ namespace WinFormTutorial
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = textBox1.Text;
+            textBox1.Text = "";
+        }
     }
 }
