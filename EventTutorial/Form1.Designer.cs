e59cf535ebacada43ed6fe1b1abbcda6fdd28f2b
sonohoshi <yd111544@gmail.com>
2020-10-13 19:15:35 +0900

    Dynamic make control

--- EventTutorial/Form1.Designer.cs
+++ EventTutorial/Form1.Designer.cs
@@ -31,8 +31,7 @@ namespace EventTutorial
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources =
-                new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Timers.Timer();
             ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).BeginInit();
@@ -43,9 +42,9 @@ namespace EventTutorial
             // 
             this.pictureBox1.Image = ((System.Drawing.Image) (resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.InitialImage = ((System.Drawing.Image) (resources.GetObject("pictureBox1.InitialImage")));
-            this.pictureBox1.Location = new System.Drawing.Point(274, 131);
+            this.pictureBox1.Location = new System.Drawing.Point(191, 328);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(219, 209);
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -70,9 +69,10 @@ namespace EventTutorial
             ((System.ComponentModel.ISupportInitialize) (this.timer1)).EndInit();
             this.ResumeLayout(false);
         }
+        
         private System.Timers.Timer timer1;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox[] pictureBox2;
+        private PictureBox[] pictureBox2;
 
         #endregion
         
@@ -89,7 +89,7 @@ namespace EventTutorial
                 // 
                 this.pictureBox2[i].Load(@"C:\Users\82108\WorkSpace\WinForm_Tutorial\EventTutorial\Resources\char.gif");
                 this.pictureBox2[i].Location = new System.Drawing.Point(581 - i * 80, 158);
-                this.pictureBox2[i].Name = "this.pictureBox2[i]Box2";
+                this.pictureBox2[i].Name = $"this.pictureBox2[{i}]Box2";
                 this.pictureBox2[i].Size = new System.Drawing.Size(80, 80);
                 this.pictureBox2[i].SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
                 this.pictureBox2[i].TabIndex = 3;
@@ -97,7 +97,23 @@ namespace EventTutorial
                 ((System.ComponentModel.ISupportInitialize) (this.pictureBox2[i])).EndInit();
                 this.Controls.Add(this.pictureBox2[i]);
             }
+        }
 
+        private PictureBox MakeBullet(int x,int y)
+        {
+            System.Windows.Forms.PictureBox pictureBox3;
+            pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize) (pictureBox3)).BeginInit();
+            pictureBox3.Load(@"C:\Users\82108\WorkSpace\WinForm_Tutorial\EventTutorial\Resources\bullet.png");
+            pictureBox3.Location = new System.Drawing.Point(x, y);
+            pictureBox3.Name = "pictureBox2";
+            pictureBox3.Size = new System.Drawing.Size(39, 37);
+            pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 1;
+            pictureBox3.TabStop = false;
+            this.Controls.Add(pictureBox3);
+            ((System.ComponentModel.ISupportInitialize) (pictureBox3)).EndInit();
+            return pictureBox3;
         }
     }
 }
--- EventTutorial/Form1.cs
+++ EventTutorial/Form1.cs
@@ -13,34 +13,39 @@ namespace EventTutorial
 {
     public partial class Form1 : Form
     {
+        private List<PictureBox> bulletList;
+        
         public Form1()
         {
             InitializeComponent();
             MakePictureBoxArray(10);
+            bulletList = new List<PictureBox>();
         }
 
         private void Form1_MouseClick(object sender, MouseEventArgs e)
         {
+            bulletList.Add(MakeBullet(e.X, e.Y));
             pictureBox1.Left = e.X;
             pictureBox1.Top = e.Y;
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            switch (e.KeyCode)
+            if (e.KeyCode == Keys.W)
             {
-                case Keys.W:
-                    pictureBox1.Top -= 5;
-                    break;
-                case Keys.S:
-                    pictureBox1.Top += 5;
-                    break;
-                case Keys.A:
-                    pictureBox1.Left -= 5;
-                    break;
-                case Keys.D:
-                    pictureBox1.Left += 5;
-                    break;
+                pictureBox1.Top -= 10;
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                pictureBox1.Top += 10;
+            }
+            if (e.KeyCode == Keys.A)
+            {
+                pictureBox1.Left -= 10;
+            }
+            if(e.KeyCode == Keys.D)
+            {
+                pictureBox1.Left += 10;
             }
         }
 
