82cab425c98551aa5c1b3444c84b318474886c9e
sonohoshi <yd111544@gmail.com>
2020-11-26 23:16:27 +0900

    C# with python tutorials

---
+++ WithPython/CsharpWithPython/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CsharpWithPython
+{
+    public partial class Form1 : Form
+    {
+        private ProcessStartInfo startInfo = null;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Equals("")) return;
+            try
+            {
+                string file = "test.py";
+                
+                startInfo = new ProcessStartInfo("python.exe");
+                startInfo.RedirectStandardError = true;
+                startInfo.RedirectStandardOutput = true;
+                startInfo.UseShellExecute = false;
+                startInfo.WindowStyle = ProcessWindowStyle.Minimized;
+
+                System.IO.File.WriteAllText(file, textBox1.Text, Encoding.UTF8);
+                
+                startInfo.Arguments = file;
+                Process process = new Process();
+                process.StartInfo = startInfo;
+                process.Start();
+                StreamReader reader = process.StandardError;
+                MessageBox.Show(reader.ReadToEnd());
+                reader = process.StandardOutput;
+                MessageBox.Show(reader.ReadToEnd());
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+    }
+}
